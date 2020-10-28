09478f1fc5e89d7dc2c47a99576e410c874d9b8d
Umar Mohammad <16259131+umarwmode@users.noreply.github.com>
2020-10-27 23:42:17 -0600

    Create AboutPage.spec.ts

---
+++ cypress/integration/specs/AboutPage.spec.ts
@@ -0,0 +1,9 @@
+//Navigate to https://shift.com/about
+
+//There is no buttons on this page
+//But verify all the text on this page
+//Veify the header text
+//Verify the paragraph text
+//Verify the link in the press release section
+
+//I know you asked for specific things but there is not much in this page. Verify and assert whatever you can
