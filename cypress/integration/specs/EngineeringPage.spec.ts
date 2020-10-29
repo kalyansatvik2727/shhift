1055c823a84fe60d3c1049b12931aeefe33f1b5d
Umar Mohammad <16259131+umarwmode@users.noreply.github.com>
2020-10-29 17:31:39 -0600

    Create EngineeringPage.spec.ts

---
+++ cypress/integration/specs/EngineeringPage.spec.ts
@@ -0,0 +1,16 @@
+// Navigate to https://shift.com/engineering
+
+// assert the header text and button are displayed
+// assert the header Marketplace & consumer products and the text under it
+//assert that clicking on A self-driven purchase process open a modal
+// assert the header Logistics platforms and the text under it
+//assert that clicking on Efficient field staffing open a modal and verify the modal text
+// asert that clicking on Extended workflow app open a modal and verifuy the modal text
+// assert the header Pricing engine and text under it
+// asert that clicking on Pricing engine open a modal and verify the modal text
+
+// assert the header Our Stack and text under it
+// assert the header Meet the Team and text under it
+// asssert the button read our experiences is displayed and clickable
+//assert the header Join us and text under it
+// assert the header Engineering, Product, and Design and text under it
