42b3c82c507049ba8fb3d7718f9ccec224fb5404
Umar Mohammad <16259131+umarwmode@users.noreply.github.com>
2020-10-29 17:40:41 -0600

    Create ConciergePage.spec.ts

---
+++ cypress/integration/specs/ConciergePage.spec.ts
@@ -0,0 +1,22 @@
+// Navigate to https://shift.com/concierges
+// Assert the header and text under it
+// Assert the button Apply Now is displayed and clickable
+// Assert the What is Shift header and text under it
+// Assert the Concierge Core Responsibilities header and text under it
+// Assert the header Flexible Schedule and text under it
+We do our best to set a schedule that works well for you. We offer part time and full time positions.
+
+// assert the header Reliable Earnings and text under it
+Hourly, bi-weekly pay, starting at $15/hour with performance-based increases and promotional opportunities available for rockstars.
+
+// assert the header Growth and Development and text under it 
+Gain sales and customer service experience. At Shift, we support each other and work with you to find your track.
+
+// assert the header Fast Support and text under it
+You’ll have backup from our dedicated Support Teams. Have questions? They’re there.
+
+// assert the header Why become a Concierge? and text under it
+
+//assert that the photo logo is clickable and the feedback text above it chnages 
+// assert the FAQ header
+//assert that 8 questions can be clicked, expanded and assert the text in each question
