476a5c4ea65474be4d9ecc260c99a9ce2cf58e46
Umar Mohammad <16259131+umarwmode@users.noreply.github.com>
2020-10-29 17:24:27 -0600

    Create CarLoanCalculator.spec.ts

---
+++ cypress/integration/specs/CarLoanCalculator.spec.ts
@@ -0,0 +1,5 @@
+//Navigate to https://shift.com/car-loan-calculator
+
+// Check all header text and text
+// Check that all buttons can be clicked 
+//Check the bottom two buttons are dispalyed and clickable
