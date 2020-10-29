c11e28d83a5284a58f48403c31bc6eccf236ec32
Umar Mohammad <16259131+umarwmode@users.noreply.github.com>
2020-10-29 17:20:08 -0600

    Create ProtectionPage.spec.ts

---
+++ cypress/integration/specs/ProtectionPage.spec.ts
@@ -0,0 +1,31 @@
+//Navigate to https://shift.com/protection
+//Assert the text of the header
+//Assert the two header: Vehicle Protection plan & Gap Coverage
+//Assert the text below it
+//Check that two button are displayed and clickable
+//Assert the two buttons text
+//Assert that this text is present:
+
+//Protection against the unexpected
+//Key lockout
+// Get reimbursed when you need a locksmith to save you from a lockout jam
+
+//Rental reimbursement
+// Get reimbursed for a rental while your car’s in the shop
+
+//Free towing
+//No-charge tow service to any ASE-certified repair facility within 150 miles
+
+//Flat tire change
+//Get a lift to the tire shop if your spare won’t work
+
+//Battery jump
+//Need some juice? Have a technician come to you to help get your car started
+
+
+//Assert this header and text:
+
+//GAP COVERAGE
+//Never owe more than your car is worth
+
+// assert this text as well:Talk to your Shift advisor or simply find a vehicle and add gap coverage after booking a test drive
