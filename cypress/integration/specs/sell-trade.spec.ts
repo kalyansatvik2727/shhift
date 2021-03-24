2ab043bb3fca1a43f140f199ba054c01955b3d4c
unknown <satvik.desamala@gmail.com>
2021-03-24 10:02:06 +0530

    sell-trade.spec.js is fixed

--- cypress/integration/specs/sell-trade.spec.ts
+++ cypress/integration/specs/sell-trade.spec.ts
@@ -1,15 +1,19 @@
 import SellTradePage from '../pages/sell-trade.page'
 import Actions from '../pages/common-actions'
 import Utilities from '../pages/utilities'
+import CommonActions from '../../support/CommonActions'
+const commonActions = new CommonActions()
 
 const sellTradePage = new SellTradePage()
 const actions = new Actions()
 const utils = new Utilities()
 
 describe('Sell or Trade Cars Page Test', () => {
+  before(() => {
+    commonActions.envLogin('/sell-my-car')
+  })
 
   it('Sell Trade Car', () => {
-    cy.visit('https://shift.com/sell-my-car');
     sellTradePage.selectYear('2015');
     sellTradePage.selectMake('Honda');
     sellTradePage.selectModel('Civic');
@@ -18,31 +22,34 @@ describe('Sell or Trade Cars Page Test', () => {
     sellTradePage.enterMileage('1000');
     sellTradePage.selectTrim('SE');
     sellTradePage.selectExteriorColor('Black');
+    cy.get('[value="LienStatusLease"] > .TypographyParagraph__root').click()
     //sellTradePage.selectTransmission('5-Speed Manual');
-    sellTradePage.isLeased('No');
+    // sellTradePage.isLeased('No');
     actions.clickOnContinue('Continue');
     actions.clickOnContinue('Continue');
-    cy.get('button[value=no]').each(($e, index,$list) => {
+    cy.get('button[value=no]').each(($e, index, $list) => {
       cy.get('button[value=no]')
         .eq(index)
         .click();
     });
 
     sellTradePage.clickGetMyOfferButton('Get');
-    cy.get('div[id*=Pick] span',{timeout:100000}).eq(0).invoke('text').then((dollar)=>{
-        cy.log(dollar)
-        const value = utils.splitDollar(dollar)
-        cy.log(value)
-        const dollarAmount = parseInt(value)
-        if(!(value>0)){
-          cy.log('Invalid borrow amount displayed '+value)
-        }
-        else{
-              cy.log('borrow amount is greater than zero: '+value)
+
+  });
+
+  it('Validate Dollar Value', () => {
+    cy.get('div[id*=Pick] span', { timeout: 100000 }).eq(0).invoke('text').then((dollar) => {
+      cy.wrap(utils.splitDollar(dollar)).then((value) => {
+        if (parseInt(value) <= 0) {
+          cy.log('Invalid borrow amount displayed')
+        } else {
+          cy.log('borrow amount is greater than zero: ' + value)
         }
       })
 
-  });
+    })
+
+  })
 
 
 });
