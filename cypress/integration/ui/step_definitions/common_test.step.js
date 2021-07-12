83234253fb1fa96b12aaf1c8863e7f30260c3b69
unknown <satvik.desamala@gmail.com>
2021-07-12 08:10:50 +0530

    Shift Home Page validations automation is complete for forcebase env

--- cypress/integration/ui/step_definitions/common_test.step.js
+++ cypress/integration/ui/step_definitions/common_test.step.js
@@ -2,6 +2,10 @@ import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
 import CommonActions from '../pages/CommonActions'
 const commonActions = new CommonActions()
 
+Given('I navigate to shift home page with forcebase url and required login', () => {
+    commonActions.envLogin('/')
+})
+
 And('I validate Modal {string}', (text) => {
     cy.get('div[class*=Modal__is-open]').contains(text)
 });
