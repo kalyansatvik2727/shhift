c8ee65321c167dc6ec42953033f33a53b8af41d6
unknown <satvik.desamala@gmail.com>
2021-07-27 09:49:35 +0530

    Modified vehicleDescription page

--- cypress/integration/ui/step_definitions/common_test.step.js
+++ cypress/integration/ui/step_definitions/common_test.step.js
@@ -3,30 +3,30 @@ import CommonActions from '../pages/CommonActions'
 const commonActions = new CommonActions()
 
 Given('I navigate to shift home page with forcebase url and required login', () => {
-    commonActions.envLogin('/')
+    return commonActions.envLogin('/')
 })
 
 And('I validate Modal {string}', (text) => {
-    cy.get('div[class*=Modal__is-open]').contains(text)
+    return cy.get('div[class*=Modal__is-open]').contains(text)
 });
 
 And('I close Modal', (text) => {
-    cy.get('div[class*=Modal__close]').click()
+    return cy.get('div[class*=Modal__close]').click()
 });
 
 And('I validate {string} section', (text) => {
-    cy.contains(text)
+    return cy.contains(text)
 });
 
 And('I validate {string} text', (text) => {
-    cy.contains(text)
+    return cy.contains(text)
 });
 
 And('I validate {string} link', (text) => {
-    cy.contains(text)
+    return cy.contains(text)
 });
 
 And('I click on {string} link', (text) => {
-    cy.contains(text).click()
+    return cy.contains(text).click().wait(5000)
 });
 
--- cypress/integration/ui/step_definitions/vehicleDescription_test.step.js
+++ cypress/integration/ui/step_definitions/vehicleDescription_test.step.js
@@ -8,28 +8,29 @@ const commonActions = new CommonActions()
 const util = new Utilities()
 
 Given(/^I navigate to shift home page with required login$/, () => {
-	cy.visit('https://shift.com/')
+    cy.visit('https://shift.com/')
 });
 
 And(/^I click on Shop Cars link$/, () => {
-	cy.contains('Shop cars').click()
+    cy.contains('Shop cars').click()
 });
 
 And(/^I select any random car$/, () => {
-	cy.get('a div img').eq(0).click()
+    cy.get('a div img').eq(0).click()
 });
 
-Then(/^I validate vehicle description page$/, () => {
-    cy.contains('Free 7-day returns')
-    cy.contains('30-day warranty')
-    cy.contains('No-Contact Test Drives')
-});
+// Then(/^I validate vehicle description page$/, () => {
+//     cy.contains('Free 7-day returns')
+//     cy.contains('30-day warranty')
+//     cy.contains('No-Contact Test Drives')
+// });
 
 
-And(/^I click on See all specs$/, () => {
-    cy.contains('See all specs').click()
-});
+// And(/^I click on See all specs$/, () => {
+//     cy.contains('See all specs').click()
+//     cy.wait(5000)
+// });
 
-And(/^I click on See all features$/, () => {
-    cy.contains('See all features').click()
-});
+// And(/^I click on See all features$/, () => {
+//     cy.contains('See all features').click()
+// });
