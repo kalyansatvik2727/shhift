9a137deba34766b0e6b4821b5a27beba4e5fd78d
unknown <satvik.desamala@gmail.com>
2021-08-04 21:58:04 +0530

    sellerFlow.feature is fixed

---
+++ cypress/integration/ui/pages/vdp_page.js
@@ -0,0 +1,29 @@
+class VDPPage {
+
+	verifyVDPPage() {
+		cy.url().should('include', '/car');
+		cy.get('div', {timeout:10000}).contains('Test drive at home').should('be.visible');
+		cy.get('button').contains('Book test drive at home');
+		cy.get('div', {timeout:10000}).contains('Test drive at Shift').should('be.visible');
+		cy.get('button').contains('Book test drive at Shift');
+		cy.get('#VDPBuyOnlineTabScreenerId').contains('Buy online');
+	}
+
+	goToBuyOnlineTab(){
+		cy.get('#VDPBuyOnlineTabScreenerId').click();
+	}
+
+	buyOnline(){
+		const buyOnline = cy.get('button').contains('Buy online now');
+		buyOnline.click();
+	}
+
+	verifyBuyOnlinePage(){
+		cy.url().should('include', '/buy-online');
+		cy.get('div', {timeout:10000}).contains('Buy this Honda today!').should('be.visible');
+		cy.get('div', {timeout:10000}).contains('Shipping Address').should('be.visible');
+		cy.get('div', {timeout:10000}).contains('Billing information').should('be.visible');
+	}
+}
+
+export default VDPPage;
---
+++ cypress/integration/ui/pages/vehicleHistory_page.js
@@ -0,0 +1,89 @@
+class VehicleHistoryPage {
+	// Vehicle History
+
+	verifyQ1(){
+		return cy.get('div').contains("1. Does your car have any issues that prevent it from starting or driving?");
+	}
+
+	selectQ1Yes(){
+		return cy.get('label[for="quote_flow_condition_issues_starting_or_driving_yes"]', {timeout:10000}).click({ force: true });
+	}
+
+	selectQ1No(){
+		return cy.get('label[for="quote_flow_condition_issues_starting_or_driving_no"]', {timeout:10000}).click({ force: true });
+	}
+
+	verifyQ2(){
+		return cy.get('div').contains("2. Does your car have any mechanical issues or persistent dashboard warning lights?");
+	}
+
+	selectQ2Yes(){
+		return cy.get('label[for="quote_flow_condition_has_warning_lights_yes"]', {timeout:10000}).click({ force: true });
+	}
+
+	selectQ2No(){
+		return cy.get('label[for="quote_flow_condition_has_warning_lights_no"]', {timeout:10000}).click({ force: true });
+	}
+
+	verifyQ3(){
+		return cy.get('div').contains("3. Does your car have any aftermarket modifications?");
+	}
+
+	selectQ3Yes(){
+		return cy.get('label[for="quote_flow_condition_has_aftermarket_parts_yes"]', {timeout:10000}).click({ force: true });
+	}
+
+	selectQ3No(){
+		return cy.get('label[for="quote_flow_condition_has_aftermarket_parts_no"]', {timeout:10000}).click({ force: true });
+	}
+
+	verifyQ4(){
+		return cy.get('div').contains("4. Has your car been in any accidents?");
+	}
+
+	selectQ4Yes(){
+		return cy.get('label[for="quote_flow_condition_accidents_yes"]', {timeout:10000}).click({ force: true });
+	}
+
+	selectQ4No(){
+		return cy.get('label[for="quote_flow_condition_accidents_no"]', {timeout:10000}).click({ force: true });
+	}
+
+	verifyQ5(){
+		return cy.get('div').contains("5. Does your car have any persistent odors?");
+	}
+
+	selectQ5Yes(){
+		return cy.get('label[for="quote_flow_condition_persistent_odors_yes"]', {timeout:10000}).click({ force: true });
+	}
+
+	selectQ5No(){
+		return cy.get('label[for="quote_flow_condition_persistent_odors_no"]', {timeout:10000}).click({ force: true });
+	}
+	
+	verifyQ6(){
+		return cy.get('div').contains("6. Do you have a loan or lease on this vehicle?");
+	}
+
+	selectQ6Yes(){
+		return cy.get('label[for="quote_flow_condition_loan_or_lease_yes"]', {timeout:10000}).click({ force: true });
+	}
+
+	selectLoan(){
+		return cy.get('input[id="loan_or_lease_LienStatusLoan"]', {timeout:10000}).click({ force: true });
+	}
+
+	selectLease(){
+		return cy.get('input[id="loan_or_lease_LienStatusLease"]', {timeout:10000}).click({ force: true });
+	}
+
+	selectQ6No(){
+		return cy.get('label[for="quote_flow_condition_loan_or_lease_no"]', {timeout:10000}).click({ force: true });
+	}
+
+	verifyQ7(){
+		return cy.get('div').contains("7. What’s your license plate? (Optional)");
+	}
+}
+
+export default VehicleHistoryPage;
